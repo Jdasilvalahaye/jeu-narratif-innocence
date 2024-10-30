49446c1e8376cbdee9a3309690de63a6cd7e2fb5
Jonathan Dasilva Lahaye <jonathandl@mbp-de-jonathan.home>
2024-10-30 14:39:08 +0100

    warrior story correction

--- js/story/warriorStory.js
+++ js/story/warriorStory.js
@@ -1,13 +1,13 @@
 export const warriorScene = {
   warriorChoice: {
-    text: "Revêtu de votre armure de plaque, épée en main et bouclier solidement attaché, vous sortez du refuge et commencez à arpenter le sentier enneigé. Les flocons tourbillonnent autour de vous et le silence de la forêt vous enveloppe. Après quelques minutes de marche, le chemin se divise en deux. À droite, le blizzard s’intensifie, obscurcissant la vue à quelques mètres. À gauche, le chemin semble plus dégagé, et vous distinguez au loin une montagne imposante, à plusieurs heures de marche. Quel sentier décidez-vous de suivre ?",
+    text: "Revêtu de votre armure de plaque, épée en main et bouclier solidement attaché, vous sortez du refuge et commencez à arpenter le sentier enneigé. Les flocons tourbillonnent autour de vous et le silence de la forêt vous enveloppe. Après quelques minutes de marche, le chemin se divise en deux. À gauche, le blizzard s’intensifie, obscurcissant la vue à quelques mètres. À droite, le chemin semble plus dégagé, et vous distinguez au loin une montagne imposante, à plusieurs heures de marche. Quel sentier décidez-vous de suivre ?",
     choices: [
       {
-        text: "Prendre le sentier de droite",
+        text: "Prendre le sentier de gauche",
         nextScene: "rencontre loup",
       },
       {
-        text: "Prendre le sentier de gauche",
+        text: "Prendre le sentier de droite",
         nextScene: "rencontre paysan",
       },
     ],
